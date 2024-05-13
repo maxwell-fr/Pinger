52a10a5fb1080694ec2f0c0efab2b255e8285852
Maxwell French <maxwell@frenchnet.org>
2024-05-13 18:20:32 -0400

    Testing with ping-rs, which seems simpler and a bit closer to "the metal".

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use std::net::IpAddr;
 use std::time::Duration;
+use ping_rs::*;
+const PING_OPTS: PingOptions = PingOptions { ttl: 128, dont_fragment: true };
+const TIMEOUT: Duration = Duration::from_secs(5);
 
 fn main() {
     let ip = IpAddr::from([1,1,1,1]);
     println!("Pinging {ip}...");
+    let data  = [8; 8];
 
-    let timeout = Some(Duration::new(5,0));
-    let res = ping::ping(ip, timeout, None, None, None, None);
+    let res = send_ping(&ip, TIMEOUT, &data, Some(&PING_OPTS));
 
-    println!("Success? {}", res.is_ok());
+    println!("Success? {} {:?}", res.is_ok(), &res.unwrap());
 }
