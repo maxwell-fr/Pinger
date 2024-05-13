d46a47e2f1944c36bb20bdb27c14db2cc7d4a3b2
Maxwell French <maxwell@frenchnet.org>
2024-05-13 18:25:40 -0400

    Clean up result check and print rtt explicitly.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
 use std::net::IpAddr;
 use std::time::Duration;
 use ping_rs::*;
+
 const PING_OPTS: PingOptions = PingOptions { ttl: 128, dont_fragment: true };
 const TIMEOUT: Duration = Duration::from_secs(5);
 
 fn main() {
-    let ip = IpAddr::from([1,1,1,1]);
+    let ip = IpAddr::from([1, 1, 1, 1]);
     println!("Pinging {ip}...");
-    let data  = [8; 8];
+    let data = [8; 8];
 
     let res = send_ping(&ip, TIMEOUT, &data, Some(&PING_OPTS));
 
-    println!("Success? {} {:?}", res.is_ok(), &res.unwrap());
+    match res {
+        Ok(r) => {
+                println!("Success! rtt {:?}", r.rtt)
+            }
+        Err(_) => {
+            println!("Failed!")
+        }
+    }
 }
